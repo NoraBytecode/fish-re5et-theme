022221a098e00659922b5d7cbc0ccd0ff210f5e4
Nora <mail@norabyte.net>
2026-06-22 01:14:50 +0200

    Expand title to match zsh version

--- fish_title.fish
+++ fish_title.fish
@@ -2,5 +2,5 @@ function fish_title
   # Customize the title bar of the terminal window.
   # This does not affect tmux. Look at fish_right_prompt for what actually
   # affects it.
-  echo '['(prompt_pwd)']'
+  echo -s (whoami) '@' (hostname| cut -d . -f 1) ':' (prompt_pwd)
 end
