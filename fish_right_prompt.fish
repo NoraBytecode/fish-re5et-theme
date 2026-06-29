943b1e17707ab222385723f1908ca80c7db95876
Nora <mail@norabyte.net>
2026-06-29 20:16:20 +0200

    fix: remove spaces in window title

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,21 +1,26 @@
 function fish_right_prompt -d "Write out the right prompt"
-	set -l last_status $status
+    set -l last_status $status
 
-	if test $last_status -ne 0
-		set_color --bold red
-		echo -ns $last_status ' :( '
-		set_color normal
-	end
+    if test $last_status -ne 0
+        set_color --bold red
+        echo -ns $last_status ' :( '
+        set_color normal
+    end
 
-	set line (whoami) '@' (hostname| cut -d . -f 1) ':' (prompt_pwd)
+    set line "$(whoami)@$(hostname| cut -d . -f 1):$(prompt_pwd)"
 
-	# tmux, screen title
-	switch $TERM;
-	case xterm'*' vte'*';
-	printf "\033]0;[$line]\007";
-	case screen'*';
-	printf "\033k[$line]\033\\";
-	end
+    # tmux, screen title
+    switch $TERM
 
-	echo -ns (date "+%D")" - "(date "+%l:%M %p")
+        case xterm'*' vte'*'
+
+            printf "\033]0;[$line]\007"
+
+        case screen'*'
+
+            printf "\033k[$line]\033\\"
+
+    end
+
+    echo -ns (date "+%D")" - "(date "+%l:%M %p")
 end
